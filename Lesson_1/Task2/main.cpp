40061099917276f3b57c401f3574f78aebcb5810
Oleksandr Polyah <sasha1polyah@gmail.com>
2022-04-24 22:53:24 +0300

    Рисунок ascii-art

---
+++ Lesson_1/Task2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "     _______ _______" << endl
+         << "    /       Y       \\"   << endl
+         << "   / ~ ~ ~~ | ~~~ ~~ \\" << endl
+         << "  /________.|.________\\ " << endl
+         << "`----------`-'----------'" << endl;
+}
