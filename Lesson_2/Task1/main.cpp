742fde3390196cc6ad1524955b60a5c127e0e54b
Oleksandr Polyah <sasha1polyah@gmail.com>
2022-05-03 19:49:20 +0300

    Исправил задание с рамкой

--- Lesson_2/Task1/main.cpp
+++ Lesson_2/Task1/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 
 using namespace std;
-
 int main()
 {
     int size = 0;
-    for(size )
+    cout << "Size = ";
+    cin >> size;
+
+    for (int i = 0;i < size;i++)
+    {
+        for (int j = 0;j < size;j++)
+        {
+            if (i != 0 && i != size - 1 && j != 0 && j != size - 1)
+			{
+				cout << " ";
+			}
+			else
+			{
+				cout << "*";
+			}
+		}
+		cout << endl;
+	}
 }
